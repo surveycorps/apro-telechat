2a810c7669b555d9e6103958681b932d6d5c003d
Alain E Hernandez <alainh@plus.seas.upenn.edu>
2015-04-21 01:50:31 -0400

    Reverted in_all back to working command flow

--- robot/in_all.sh
+++ robot/in_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget -q 'http://10.0.0.14:5051/video' -O video.mjpeg &
+
+(wget -q 'http://10.0.0.14:5051/video' -O video.mjpeg || wget -q 'http://10.0.0.2:8080/?action=stream' -O video.mjpeg) &
 mplayer -fs -noconsolecontrols -quiet -demuxer 35 video.mjpeg  
 
